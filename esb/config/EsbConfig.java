dd4dfee3ebe708735f2eb3a8fe648849b9d742dd
Gian132004 <giannicolaiduhan@gmail.com>
2025-03-02 05:30:13 -0800

    gg

--- esb/config/EsbConfig.java
+++ esb/config/EsbConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class ESBConfig {
+public class EsbConfig {
     @Bean
     public CamelContext camelContext() {
         return new DefaultCamelContext();
--- esb/src/main/java/EsbApplication.java
+++ esb/src/main/java/EsbApplication.java
@@ -1,11 +1,13 @@
-package esb;
+package esb; // Add this if it's inside src/main/java/esb/
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ESBApplication {
+public class EsbApplication {
     public static void main(String[] args) {
-        SpringApplication.run(ESBApplication.class, args);
+        SpringApplication.run(EsbApplication.class, args);
+
+        
     }
 }
